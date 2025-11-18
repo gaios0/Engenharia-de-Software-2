b65471e90ad2715106997acbb2dea1801598fde4
gaios0 <130511397+gaios0@users.noreply.github.com>
2025-11-18 11:08:27 -0300

    Add files via upload

---
+++ src/persistencia/database.js
@@ -0,0 +1,62 @@
+<<<<<<< HEAD
+// src/persistencia/database.js
+const { Pool } = require('pg');
+
+class database {
+    static #pool = null;
+
+    static getPool() {
+        if (!this.#pool) {
+            this.#pool = new Pool({
+                user: 'postgres',
+                host: 'localhost',
+                database: 'clave_db',
+                password: 'sua_senha', // SUA SENHA AQUI
+                port: 5432,
+            });
+            console.log('Pool de conexões com o PostgreSQL estabelecido!');
+        }
+        return this.#pool;
+    }
+
+    static async closePool() {
+        if (this.#pool) {
+            await this.#pool.end();
+            console.log('Pool de conexões com o banco fechado.');
+            this.#pool = null;
+        }
+    }
+}
+
+=======
+// src/persistencia/database.js
+const { Pool } = require('pg');
+
+class database {
+    static #pool = null;
+
+    static getPool() {
+        if (!this.#pool) {
+            this.#pool = new Pool({
+                user: 'postgres',
+                host: 'localhost',
+                database: 'clave_db',
+                password: 'sua_senha', // SUA SENHA AQUI
+                port: 5432,
+            });
+            console.log('Pool de conexões com o PostgreSQL estabelecido!');
+        }
+        return this.#pool;
+    }
+
+    static async closePool() {
+        if (this.#pool) {
+            await this.#pool.end();
+            console.log('Pool de conexões com o banco fechado.');
+            this.#pool = null;
+        }
+    }
+}
+
+>>>>>>> 34ba386cc9e0d9669e4166a584f8edc6bdbf0446
+module.exports = database;
